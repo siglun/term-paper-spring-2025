2d746de132a531ba8617586b7769ffa7dc90cf00
Sigfrid Lundberg <siggelundberg@gmail.com>
2025-01-24 16:18:27 +0100

    added figur1.ms

--- externalist-theories-of-empirical-knowledge/build.sh
+++ externalist-theories-of-empirical-knowledge/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-groff -U -R -e  -m pdfpic -m pdfmark -ms -k  -s -t -P-pa4 -Tpdf parameters.ms notes.ms back-matter.ms > notes.pdf
+groff -U -R -s -e -p  -m pdfpic -m pdfmark -ms -k  -s -t -P-pa4 -Tpdf parameters.ms notes.ms back-matter.ms > notes.pdf
 
 
 
