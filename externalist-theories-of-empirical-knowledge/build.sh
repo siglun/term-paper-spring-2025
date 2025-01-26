9f657e4a01bc992108e563d4336d3f7402746bc4
Sigfrid Lundberg <siggelundberg@gmail.com>
2025-01-26 16:41:21 +0100

    added a folder version of the text

--- externalist-theories-of-empirical-knowledge/build.sh
+++ externalist-theories-of-empirical-knowledge/build.sh
@@ -2,5 +2,10 @@
 
 groff -U -R -s -e -p  -m pdfpic -m pdfmark -ms -k  -s -t -P-pa4 -Tpdf parameters.ms notes.ms back-matter.ms > notes.pdf
 
+pdf2ps notes.pdf
+
+psbook -s4 notes.ps | psnup -2 > folder.ps
+ps2pdf folder.ps
+
 
 
