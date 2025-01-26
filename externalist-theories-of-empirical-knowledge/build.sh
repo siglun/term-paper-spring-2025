f748d336b209cc88a22977556e6fcfaa348a6be1
Sigfrid Lundberg <siggelundberg@gmail.com>
2025-01-26 18:51:53 +0100

    added qr.pdf

--- externalist-theories-of-empirical-knowledge/build.sh
+++ externalist-theories-of-empirical-knowledge/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+qrencode -l L -v 1 -s 1 -d 300 -t EPS -o qr.eps "https://github.com/siglun/term-paper-spring-2025/blob/main/externalist-theories-of-empirical-knowledge/notes.pdf"
+
+epstopdf  qr.eps
+rm qr.eps
+
 groff -U -R -s -e -p  -m pdfpic -m pdfmark -ms -k  -s -t -P-pa4 -Tpdf parameters.ms notes.ms back-matter.ms > notes.pdf
 
 pdf2ps notes.pdf
@@ -8,3 +14,4 @@ psbook -s4 notes.ps | psnup -2 > folder.ps
 ps2pdf folder.ps
 
 rm folder.ps notes.ps
+
