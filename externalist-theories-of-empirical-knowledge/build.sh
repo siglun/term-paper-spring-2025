bcd303cd6e33369d600ae20db122f31c92912df1
Sigfrid Lundberg <siggelundberg@gmail.com>
2025-01-26 22:05:32 +0100

    a more logical start. qr code works.

--- externalist-theories-of-empirical-knowledge/build.sh
+++ externalist-theories-of-empirical-knowledge/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-qrencode -l L -v 1 -s 1 -d 300 -t EPS -o qr.eps "https://github.com/siglun/term-paper-spring-2025/blob/main/externalist-theories-of-empirical-knowledge/notes.pdf"
+qrencode -l H -v 1 -s 3 -d300 -t EPS -o qr.eps "https://github.com/siglun/term-paper-spring-2025/blob/main/externalist-theories-of-empirical-knowledge/notes.pdf"
 
 epstopdf  qr.eps
 rm qr.eps
 
-groff -U -R -s -e -p  -m pdfpic -m pdfmark -ms -k  -s -t -P-pa4 -Tpdf parameters.ms notes.ms back-matter.ms > notes.pdf
+groff -U -R -s -t -e -p  -m pdfpic -m pdfmark -ms -k  -s -t -P-pa4 -Tpdf parameters.ms notes.ms back-matter.ms > notes.pdf
 
 pdf2ps notes.pdf
 
